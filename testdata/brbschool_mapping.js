a89a12387d20ea9c2ffcc10b5bd088241666b296
Timo Homburg <timo.homburg@hs-mainz.de>
2019-04-12 15:15:13 +0200

    Comparison test

---
+++ testdata/brbschool_mapping.js
@@ -0,0 +1,11 @@
+var equivs={
+	"dienst_email":{"osm":"contact:email","wikidata":"http://www.wikidata.org/prop/direct/P968"},
+	"kreis":{"osm":"","wikidata":"http://www.wikidata.org/prop/direct/P131"},
+	"ort":{"osm":"addr:city","wikidata":""},
+	"plz":{"osm":"addr:postcode","wikidata":"http://www.wikidata.org/prop/direct/P281"},
+	"schueler":{"osm":"","wikidata":"http://www.wikidata.org/prop/direct/P2196"},
+	"schulname":{"osm":"name","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
+	"schultraeger_grp":{"osm":"operator:type","wikidata":""},
+	"strasse_hausnr":{"osm":"addr:housenumber","wikidata":"http://www.wikidata.org/prop/direct/P670"},
+	"telefonnummer":{"osm":"contact:phone","wikidata":"http://www.wikidata.org/prop/direct/P1329"}
+}
