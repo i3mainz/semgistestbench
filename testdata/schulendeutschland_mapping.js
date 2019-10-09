f4f1a2d61c141d3c46e65841b51cd1ef226625a8
Timo Homburg <timo.homburg@hs-mainz.de>
2019-10-09 20:40:19 +0200

    Schulen Deutschland

---
+++ testdata/schulendeutschland_mapping.js
@@ -0,0 +1,17 @@
+var deschools_equivs={
+	"E_Mail":{"osm":"contact:email","wikidata":"http://www.wikidata.org/entity/P968"},
+	"Homepage":{"osm":"website","wikidata":"http://www.wikidata.org/entity/P856"},
+	"kreis":{"osm":"","wikidata":"http://www.wikidata.org/entity/P131"},
+	"Ort":{"osm":"addr:city","wikidata":"http://www.wikidata.org/entity/P131"},
+	"PLZ":{"osm":"addr:postcode","wikidata":"http://www.wikidata.org/entity/P281"},
+	"Anzahl_Sch":{"osm":"","wikidata":"http://www.wikidata.org/entity/P2196"},
+	"Kurzname":{"osm":"short_name","wikidata":"http://www.wikidata.org/entity/P1813"},
+	"Name":{"osm":"name","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
+	"Traeger":{"osm":"operator:type","wikidata":""},
+	"Strasse":{"osm":"addr:street","wikidata":"http://www.wikidata.org/entity/P6375"},
+	"Bundesland":{"osm":"addr:state","wikidata":"http://www.wikidata.org/entity/P131"},
+	"Schulform":{"osm":"school","wikidata":"http://www.wikidata.org/entity/P670"},
+	"strasse_hausnr":{"osm":"addr:housenumber","wikidata":"http://www.wikidata.org/entity/P670"},
+	"Telefon":{"osm":"contact:phone","wikidata":"http://www.wikidata.org/entity/P1329"},
+	"Telefax":{"osm":"contact:fax","wikidata":"http://www.wikidata.org/entity/P2900"}
+}
